d527cc581c663ae76141209d585960b46394052d
stewartd <>
2010-05-17 16:08:38 +0000

    New script for simple end-to-end translation using translate.pl. Replaces previous translate.sh script.

--- translate.sh
+++ translate.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+# $Id$
+# @file translate.sh
+# @brief Simple end-to-end translation.
+#
+# @author Darlene Stewart
+#
 # Technologies langagieres interactives / Interactive Language Technologies
 # Inst. de technologie de l'information / Institute for Information Technology
 # Conseil national de recherches Canada / National Research Council Canada
-# Copyright 2008, Sa Majeste la Reine du Chef du Canada
-# Copyright 2008, Her Majesty in Right of Canada
+# Copyright 2010, Sa Majeste la Reine du Chef du Canada
+# Copyright 2010, Her Majesty in Right of Canada
 
+# Include NRC's bash library.
+SH_UTILS=`which sh_utils.sh`
+if [[ "${SH_UTILS}" == "" ]]; then
+   echo "ERROR: Unable to find sh_utils.sh. Is your PATH set correctly for Portage?" >&2
+   exit 1
+fi
+source ${SH_UTILS}
 
 usage() {
    for msg in "$@"; do
@@ -12,93 +25,40 @@ usage() {
    done
    cat <<==EOF== >&2
 
-Usage: translate.sh [options] source_file
+Usage: translate.sh [OPTIONS] [SOURCE_FILE]
 
-  Takes a source text file and translates it according to the current trained
-  models.
-  source_file must be tokenized and splitted one sentence per line.
+  Translate input source text from SOURCE_FILE (or STDIN) according to the 
+  currently trained models (relative to the current directory). The complete 
+  translation pipeline is run: pre-processing, tokenization, translation, 
+  truecasing (if models available), detokenization, post-processing.
+  
+  If SOURCE_FILE is not provided, input is read from STDIN. The input text
+  is assumed to contain paragraphs separated by blank lines.
+  
+  This script must be located in the root directory of the framework
+  along with the Makefile.params file from which it obtains settings.
 
 Options:
 
-  -l(ang)     language of source_file [en].
-  -h(elp)     print this help message
-  -v(erbose)  increment the verbosity level by 1 (may be repeated)
-  -d(ebug)    print debugging information
+  -do|-decode-only    translate without rescoring and confidence estimation
+  -wr|-with-rescoring translate with rescoring
+  -wc|-with-ce        translate with confidence estimation
+  -h(elp)             print this help message
+  -v(erbose)          increment the verbosity level by 1 (may be repeated)
+  -d(ebug)            print debugging information
 
 ==EOF==
 
    exit 1
 }
 
-# error_exit "some error message" "optionnally a second line of error message"
-# will exit with an error status, print the specified error message(s) on
-# STDERR.
-error_exit() {
-   for msg in "$@"; do
-      echo $msg >&2
-   done
-   echo "Use -h for help." >&2
-   exit 1
-}
-
-# Verify that enough args remain on the command line
-# syntax: one_arg_check <args needed> $# <arg name>
-# Note that this function expects to be in a while/case structure for
-# handling parameters, so that $# still includes the option itself.
-# exits with error message if the check fails.
-arg_check() {
-   if [ $2 -le $1 ]; then
-      error_exit "Missing argument to $3 option."
-   fi
-}
-
-# arg_check_int $value $arg_name exits with an error if $value does not
-# represent an integer, using $arg_name to provide a meaningful error message.
-arg_check_int() {
-   expr $1 + 0 &> /dev/null
-   RC=$?
-   if [ $RC != 0 -a $RC != 1 ]; then
-      error_exit "Invalid argument to $2 option: $1; integer expected."
-   fi
-}
-
-# arg_check_pos_int $value $arg_name exits with an error if $value does not
-# represent a positive integer, using $arg_name to provide a meaningful error
-# message.
-arg_check_pos_int() {
-   expr $1 + 0 &> /dev/null
-   RC=$?
-   if [ $RC != 0 -a $RC != 1 ] || [ $1 -le 0 ]; then
-      error_exit "Invalid argument to $2 option: $1; positive integer expected."
-   fi
-}
-
-# Print a warning message
-warn() {
-   echo "WARNING: $*" >&2
-}
-
-# Print a debug message
-debug() {
-   test -n "$DEBUG" && echo "<D> $*" >&2
-}
-
-# Print a verbose message
-verbose() {
-   level=$1; shift
-   if [[ $level -ge $VERBOSE ]]; then
-      echo "$*" >&2
-   fi
-}
-
-
-# Command line processing [Remove irrelevant parts of this code when you use
-# this template]
+# Command line processing
 VERBOSE=0
-LANGUAGE=en
 while [ $# -gt 0 ]; do
    case "$1" in
-   -l|-lang)            arg_check 1 $# $1; LANGUAGE=$2; shift;;
+   -do|-decode-only)	DECODE_ONLY=1;;
+   -wr|-with-rescoring) WITH_RESCORING=1;;
+   -wc|-with-ce)        WITH_CE=1;;
    -v|-verbose)         VERBOSE=$(( $VERBOSE + 1 ));;
    -d|-debug)           DEBUG=1;;
    -h|-help)            usage;;
@@ -109,42 +69,74 @@ while [ $# -gt 0 ]; do
    shift
 done
 
-test $# -eq 0   && error_exit "Missing the input file to translate."
-full_source=$1; shift
-source=`basename $full_source`
+ROOTDIR=`dirname $0`
+MAKEFILE_PARAMS="${ROOTDIR}/Makefile.params"
+[[ -e ${MAKEFILE_PARAMS} ]] \
+   || error_exit "Makefile.params not found in directory with translate.sh."
 
-# Make sure that the source has the framework's expected form
-# <PREFIX>_<LANGUAGE>.al
-source=${source%_${LANGUAGE}.al}_${LANGUAGE}.al
-
-# Are there superfluous arguments
-test $# -gt 0   && error_exit "Superfluous arguments $*"
-
-# Extract the prefix of this translation set.
-prefix=${source%_${LANGUAGE}.al}
-
-# Check if the inputs are of the form <stem>_<src_lang>.<langx>
-verbose 1 "Translating: $prefix"
-
-# Lets make a copy of the source file in corpora
-test -f corpora/$source || cp $full_source corpora/$source
-
-# Check if there is a valid decoding_model and if needed a valid rescoring_model
-[[ -f "models/decode/canoe.ini.cow" ]] || error_exit "You need to train a decoding model first (models/decode)"
-
-# Preprocess the corpus
-make -C corpora translate TRANSLATE_SET=$prefix 1>&2
-if [[ $? -ne 0 ]]; then
-   error_exit "Problem while preprocessing $source";
+if [[ $# -gt 0 ]]; then
+   # [[ $# -gt 0 ]] || error_exit "Missing the input file to translate."
+   SOURCE_FILE=$1; shift
+   [[ $# -eq 0 ]] || error_exit "Unexpected argument(s): $@" >&2
 fi
 
-# And decode
-make -C translate all TEST_SET=$prefix 1>&2
-if [[ $? -ne 0 ]]; then
-   error_exit "Problem while translating $source";
+[[ "${DECODE_ONLY}${WITH_RESCORING}${WITH_CE}" -gt "1" ]] \
+   && error_exit "Specify only one of -decode-only, -with-rescoring, -with-ce."
+MODE="-decode-only"
+[[ ${WITH_RESCORING} ]] && MODE="-with-rescoring"
+[[ ${WITH_CE} ]] && MODE="-with-ce"
+
+if [[ ${WITH_RESCORING} ]]; then
+   # Check if the rescoring model was set to build.
+   TEXT=`grep -E '^ *DO_RESCORING *\??=' ${MAKEFILE_PARAMS}`
+   [[ ${TEXT} =~ '= *([^ ]*)' ]] && [[ ${BASH_REMATCH[1]} == 1 ]] \
+      || warn "DO_RESCORING not enabled; rescoring model may not have been built."
 fi
 
-#echo "Your file is available in" >&2
-#cat translate/$prefix.translation
+if [[ ${WITH_CE} ]]; then
+   # Check if the confidence estimation model was set to build.
+   TEXT=`grep -E '^ *DO_CE *\??=' ${MAKEFILE_PARAMS}`
+   [[ ${TEXT} =~ '= *([^ ]*)' ]] && [[ ${BASH_REMATCH[1]} == 1 ]] \
+      || warn "DO_CE not enabled; confidence estimation model may not have been built."
+fi
 
-echo "Your translation file is available here: translate/$prefix.translation"
+# Determine the source language.
+TEXT=`grep -E '^( *|export +)SRC_LANG *\??=' ${MAKEFILE_PARAMS}`
+[[ ${TEXT} =~ '= *([^ ]*)' ]] && SRC_OPT="-src=${BASH_REMATCH[1]}"
+
+# Determine the target language.
+TEXT=`grep -E '^( *|export +)TGT_LANG *\??=' ${MAKEFILE_PARAMS}`
+[[ ${TEXT} =~ '= *([^ ]*)' ]] && TGT_OPT="-tgt=${BASH_REMATCH[1]}"
+
+# Locate the canoe.ini.cow file.
+# We assume that this translate.sh script is at the root of the framework.
+CANOE_INI="canoe.ini.cow"
+if [[ ! -e ${CANOE_INI} ]]; then
+   CANOE_INI="${ROOTDIR}/translate/canoe.ini.cow"
+   [[ -e ${CANOE_INI} ]] || ln -s "../models/decode/canoe.ini.cow" ${CANOE_INI}
+   CANOE_INI_OPT="-f=\"${CANOE_INI}\""
+fi
+
+# Determine if truecasing
+TEXT=`grep -E '^ *DO_TRUECASING *\??=' ${MAKEFILE_PARAMS}`
+if [[ ${TEXT} =~ '= *([^ ]*)' ]] && [[ ${BASH_REMATCH[1]} == 1 ]]; then
+   TPLM_CNT=0
+   for NAME in `dirname ${CANOE_INI}`/models/tc/*.tplm; do
+      [[ $(basename ${NAME}) =~ '^log.*$' ]] || TPLM_CNT=$(( $TPLM_CNT + 1 ))
+   done
+   if [[ ${TPLM_CNT} -gt 0 ]]; then
+      TC_OPT="-tctp"
+      verbose 1 "Using tightly packed truecasing model (-tctp)." >&2
+   else
+      TC_OPT="-tc"
+      verbose 1 "Using text truecasing model (-tc)." >&2
+   fi
+fi
+
+for (( V=$VERBOSE; $V>0; V=$V-1 )) ; do
+   V_OPT="$V_OPT -v"
+done
+
+run_cmd "translate.pl ${MODE} ${SRC_OPT} ${TGT_OPT} ${TC_OPT} ${CANOE_INI_OPT} ${V_OPT} ${SOURCE_FILE}"
+
+exit 0
