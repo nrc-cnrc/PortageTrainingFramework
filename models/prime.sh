3a6e472e48471717a22358e633f4649d35fc6d3d
larkins <>
2011-11-04 16:05:46 +0000

    Moved to models/.

---
+++ models/prime.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# $Id$
+
+# @file prime.sh
+# @brief Load memory map models in memory.
+#
+# @author Samuel Larkin
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2011, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2011, Her Majesty in Right of Canada
+
+
+# Usage:
+# prime.sh PrimeMode
+#    PrimeMode:
+#       - full:    load all tightly packed models in memory.
+#       - partial: load phrase tables and language models only.
+# NOTE:
+#   In both cases, we want to load the language models last since it is more
+#   crutial to have them in memory then the other models.  By loading them
+#   last, they can replace parts of the other models if there is not enough
+#   memory to hold all models at once in memory.
+
+CONTEXT=`dirname $0`
+# This can either be "full" or "partial".
+PrimeMode=$1
+
+if [[ "$PrimeMode" == "full" ]]; then
+   # Prime all models in their entirety.
+   for f in `find $CONTEXT -name \*.tppt; find $CONTEXT -name \*.tpldm; find $CONTEXT -name \*.tplm`; do
+      cat $f/* &> /dev/null
+   done
+elif [[ "$PrimeMode" == "partial" ]]; then
+   # Prime all phrase table models and language models for decoding.
+   for f in `find $CONTEXT/models/tm -name \*.tppt; find $CONTEXT/models/lm -name \*.tplm`; do
+      SIZE=$((`cat $f/* | \wc -c` / 4))
+      cat $f/* | head --byte=$SIZE &> /dev/null
+   done
+else
+   exit 3
+fi
+
+exit
