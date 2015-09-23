5b0ae02f87cb819c3850dd3fe55ab579b727b0c6
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2015-09-23 14:45:52 -0400

    prime.sh uses configtool prime_{partial,full}

--- models/prime.sh
+++ models/prime.sh
@@ -8,16 +8,15 @@
 # Technologies langagieres interactives / Interactive Language Technologies
 # Inst. de technologie de l'information / Institute for Information Technology
 # Conseil national de recherches Canada / National Research Council Canada
-# Copyright 2011, Sa Majeste la Reine du Chef du Canada /
-# Copyright 2011, Her Majesty in Right of Canada
+# Copyright 2011-2015, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2011-2015, Her Majesty in Right of Canada
 
 
 # Usage:
 # prime.sh PrimeMode
 #    PrimeMode:
 #       - full:    load all tightly packed models in memory.
-#       - partial: load phrase tables and language models only; for phrase tables, load 
-#                  only 25% of the tppt data file, but the entirety of the other tpt files.
+#       - partial: load a minimal set of tpt in memory.
 # NOTE:
 #   In both cases, we want to load the language models last since it is more
 #   crucial to have them in memory then the other models. By loading them
@@ -30,39 +29,20 @@ CONTEXT=`dirname $0`
 # This can either be "full" or "partial".
 PrimeMode=$1
 
-if [[ "$PrimeMode" == "full" ]]; then
-   # Prime all models in their entirety.
-   for f in `find $CONTEXT/ -name \*.tppt; find $CONTEXT/ -name \*.tpldm; find $CONTEXT/ -name \*.tplm`; do
-      cat $f/* &> /dev/null
-   done
-elif [[ "$PrimeMode" == "partial" ]]; then
-   # Prime all phrase table models and language models for decoding.
-   # Prime only 25% of any model.tppt/tppt file, but 100% of the other model.tppt/* files.
-   # TMs
-   for d in `find $CONTEXT/models/tm -name \*.tppt`; do
-      for f in tppt; do
-         SIZE=$((`du -b $d/$f | cut -f 1` / 4))
-         head --byte=$SIZE $d/$f &> /dev/null
-      done
-      for f in cbk src.tdx trg.repos.dat trg.tdx; do
-         cat $d/$f &> /dev/null
-      done
-   done
-   # LMs
-   for d in `find $CONTEXT/models/*lm -name \*.tplm`; do
-      for f in $d/*; do
-         cat $f &> /dev/null
-      done
-   done
+if [[ "$PrimeMode" == "full" ]] || [[ "$PrimeMode" == "partial" ]]; then
+   configtool prime_$PrimeMode canoe.ini.cow
 else
+   # This is a unknown prime mode.
    exit 3
 fi
 
 #Prime the software.
 if [[ -e $CONTEXT/ce_model.cem ]]; then
-   echo "" | $CONTEXT/soap-translate.sh -with-ce >& /dev/null
+   # Translating 'a' is a temporray hack while we properly fix translating an
+   # empty sentence with a system that has confidence estimation.
+   $CONTEXT/soap-translate.sh -with-ce >& /dev/null <<< 'a'
 else
-   echo "" | $CONTEXT/soap-translate.sh -decode-only >& /dev/null
+   $CONTEXT/soap-translate.sh -decode-only >& /dev/null <<< ''
 fi
 
 exit
